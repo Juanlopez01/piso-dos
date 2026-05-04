7282cfba9b81d83652f8bd764e1a100aa2fab085
Juan Lopez <juanpablolopez43@gmail.com>
2026-05-04 10:14:46 -0300

    mp3

--- piso2-app/proxy.ts
+++ piso2-app/proxy.ts
@@ -42,6 +42,7 @@ export async function proxy(request: NextRequest) {
             request.nextUrl.pathname.startsWith('/api/mercadopago') ||
             request.nextUrl.pathname.startsWith('/act-password') ||
             request.nextUrl.pathname.startsWith('/alquiler') ||
+            request.nextUrl.pathname.startsWith('/pago-exito') ||
             request.nextUrl.pathname.startsWith('/instalar') // 🚀 EL PASE VIP PARA MERCADO PAGO
 
         // 3. Si hay un error de token o no hay usuario, y quiere entrar a zona privada -> Al login
